f4b274bef83a6524d3f024dbf2b81f1923ec3c67
Erfankd <erfan.12.kordi@gmail.com>
2023-12-10 13:39:27 +0100

    execution ls

--- exe.c
+++ exe.c
@@ -6,35 +6,13 @@
 /*   By: ekordi <ekordi@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 14:55:02 by ekordi            #+#    #+#             */
-/*   Updated: 2023/12/07 14:09:16 by ekordi           ###   ########.fr       */
+/*   Updated: 2023/12/07 15:18:27 by ekordi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 
-
-// char	*get_cmd_path(char *cmd, char **envp)
-// {
-// 	int		i;
-// 	char	*path;
-// 	char	**paths;
-
-// 	i = 0;
-// 	while (ft_strnstr(envp[i], "PATH", 4) == 0)
-// 		i++;
-// 	paths = ft_split(envp[i], ':');
-// 	i = 0;
-// 	while (paths[i])
-// 	{
-// 		path = ft_strjoin_free(paths[i], ft_strjoin("/", cmd));
-// 		if (access(path, F_OK) == 0)
-// 			return (path);
-// 		free(path);
-// 		i++;
-// 	}
-// 	return (NULL);
-// }
 char	*cmd_path(char *cmd, char **envp)
 {
 	char	*temp;
@@ -60,7 +38,7 @@ char	*cmd_path(char *cmd, char **envp)
 		free(temp);
 		free(path);
 	}
-	ft_putstr_fd("zsh: command not found: ", 2, true);
+	ft_putstr_fd("zsh: command not found: ", 2, false);
 	ft_putstr_fd(cmd, 2, true);
 	free_arrayofstrings(paths);
 	return NULL;
@@ -70,8 +48,8 @@ void	execute(char *cmd, char **envp)
 	char	*path;
 
 	path = cmd_path(cmd, envp);
-	if (path == NULL)
-		ft_putstr_fd("Path Error", 2, true);
+	if(!path)
+		return ;
 	char *args[] = {cmd, NULL};
 	int pid1 = fork();
 	if (pid1 < 0)
